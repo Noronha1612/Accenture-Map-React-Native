3f83428754a5446a300a58383365163d16b1e0f9
NoronhaGCP <inc.691@gmail.com>
2021-02-23 22:38:20 -0300

    update Interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,3 +4,33 @@ export interface HeaderProps extends ViewProps {
     showCancel?: boolean;
     title: string;
 }
+
+export interface AccenturePoint {
+  id: number,
+  name: string,
+  country: string,
+  state: string,
+  city: string,
+  profile: string,
+  latitude: number,
+  longitude: number,
+  address: {
+    street: string,
+    complement: string,
+    number: string,
+    zipCode: string
+  },
+  describle: string,
+  contactURL: string
+}
+
+export interface IInitialMarker {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+export interface GetUnit {
+  id: number;
+}
